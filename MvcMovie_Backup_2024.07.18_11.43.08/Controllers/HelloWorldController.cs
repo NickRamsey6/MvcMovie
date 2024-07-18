9ce7853026071d8506817fbb461e277a7e209ecb
NickRamsey6 <nramseysc@gmail.com>
2024-07-18 11:55:38 -0600

    Add knedo button

---
+++ MvcMovie_Backup_2024.07.18_11.43.08/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        // Call the controller's View method
+        return View();
+        // Uses a view template to generate an HTML response
+    }
+    //
+    // GET: /HelloWorld/Welcome/
+    // Requires using System.Text.Encodings.Web;
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
--- MvcMovie_Backup_2024.07.18_11.43.08/Program.cs
+++ MvcMovie_Backup_2024.07.18_11.43.08/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
@@ -6,13 +6,9 @@ using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
-// Add framework services.
-builder.Services.AddControllersWithViews();
-	//builder.Services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-// Add Kendo UI services to the services container
-builder.Services.AddKendo();
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
